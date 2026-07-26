b5810366829547bdec2da274b8a21322ecd1af96
David Ohimai <daveohimai@gmail.com>
2026-07-26 01:28:08 +0100

    first commit

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,5 +5,5 @@ class AppConstants {
   static const onboardingCompleteKey = 'onboarding_complete';
   static const themeModeKey = 'theme_mode_dark';
   static const deliveryFee = 1500.0;
-  static const freeDeliveryThreshold = 20000.0;
+  static const freeDeliveryThreshold = 60000.0;
 }
